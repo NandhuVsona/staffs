c751b988b43620e964ab74d424df8efce12ec641
Learn Something <131090906+NandhuVsona@users.noreply.github.com>
2024-04-08 12:50:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ profileList.forEach((faculty) => {
         </section>
         </div>
         <div class="btns">
-          <a href="https://wa.link/ojtvex"><button class="emil">Message</button></a>
+          <a href="https://google.com"><button class="emil">Message</button></a>
           <button class="back">Go Back</button>
         </div>
 
